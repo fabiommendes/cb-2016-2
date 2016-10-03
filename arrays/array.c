cb443ddc4ac0a97a9f247be8502d5919fb1e5bfc
Fábio Mendes <fabiomacedomendes@gmail.com>
2016-10-03 15:52:28 -0300

    soma

--- arrays/array.c
+++ arrays/array.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 #include "array_util.h"
 
+int  sum_ints(int L[], int size) {
+    int i;
+    int S = 0;
+  
+    for (i = 0; i < size; i++) {
+        S = S + L[i];
+    }
+    
+    return S;
+}
 int main(void) {
     int fibo_list[10];
     int idx;
+    int S;
     
     fibo_list[0] = 1;
     fibo_list[1] = 1;
@@ -13,4 +24,6 @@ int main(void) {
     }
     
     print_ints(fibo_list, 10);
+    S = sum_ints(fibo_list, 10);
+    printf("soma: %d\n", S);
 }
