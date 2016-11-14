a850a195815d69e31d556d644cc40a36013765da
Fábio Mendes <fabiomacedomendes@gmail.com>
2016-11-14 15:12:10 -0200

    malloc - comentarios

--- malloc/lista.c
+++ malloc/lista.c
@@ -8,22 +8,42 @@ double* push(double *ptr, int size, double elem) {
         ptr[0] = elem;
         return ptr;
     } else {
+        // Aloca espaco para size + 1 elementos
         double *novo = malloc(sizeof(double) * (size + 1));
+        
+        // Copia valores para a nova região de memória
         for (int i=0; i < size; i++) {
             novo[i] = ptr[i];
         }
+        
+        // Acrescenta último elemento
         novo[size] = elem;
+        
+        // Libera antiga região de memória
         free(ptr);
+        
         return novo;
     }
 }
 
+/**
+ * Calcula a media dos termos de uma lista com size elementos
+ */
 double media(double *ptr, int size) {
     double S = 0;
     for (int i=0; i < size; i++) {
         S += ptr[i];
     }
     return S / size;
+}
+
+
+/**
+ * Calcula o desvio padrão dos termos de uma lista com size 
+ * elementos
+ */
+double desvpad(double *ptr, int size) {
+    // ...
 }
 
 int main(void) {
